41e79ef6e2a3e61b64c89b71d0cba19f80d5b661
pedro <peedroisac@gmail.com>
2024-04-04 10:37:07 -0300

    testanto02

---
+++ aula11/VariaveisInteiras.java
@@ -0,0 +1,20 @@
+package aula11;
+
+public class VariaveisInteiras {
+    
+    public static void main(String[] args) {
+
+        byte idade01 = 20;
+
+        short idade02 = 21;
+
+        int idade03 = 22;
+
+        System.out.println("Valor variável idade1 = "  + idade01);
+
+        System.out.println("Valor variável idade2 = "  + idade02);
+
+        System.out.println("Valor variável idade3 = "  + idade03);
+        
+    }
+}
