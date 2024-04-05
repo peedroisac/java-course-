b62c525e605296ba65519afbca2e6ed99403ccf4
pedro <peedroisac@gmail.com>
2024-04-05 11:55:59 -0300

    Main

---
+++ aula13/OperadoresRelacionais.java
@@ -0,0 +1,19 @@
+package aula13;
+
+public class OperadoresRelacionais {
+
+    public static void main(String [] args){
+
+        int valor 1 = 1;
+        int valor 2 = 2;
+
+        System.out.println();
+
+
+
+
+
+
+
+    }
+}
