01fb3a30b0c639c394ef873e5e1dc3d6efc4afd4
pedro <peedroisac@gmail.com>
2024-04-05 09:05:37 -0300

    main

--- aula11/CuriosidadeInt.java
+++ aula11/CuriosidadeInt.java
@@ -4,11 +4,11 @@ public class CuriosidadeInt {
 
     public static void main(String[] args) {
     
-        int var1 = 2147483647;
+        int var1 = 2147483647; //os numeros no java são igual roleta. dps desse valor ele volta para o primeiro numero negativo (-2147483549)
 
         int var2 = 100;
 
-        System.out.println(var1 + var2);
+        System.out.println(var1 + var2); 
 
     }
 }
