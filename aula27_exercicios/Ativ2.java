2792d3e91e1927494895b39f72946296607c5a29
pedro <peedroisac@gmail.com>
2024-05-23 15:42:48 -0300

    main

--- aula27_exercicios/Ativ2.java
+++ aula27_exercicios/Ativ2.java
@@ -9,9 +9,46 @@ public class Ativ2 {
         conta.agencia = "1234";
         conta.especial = true;
         conta.limiteEspecial = 500;
+        conta.valorEspecialUsado = 500;
         conta.saldo = -10;
 
         System.out.println("Saldo da conta " + conta.numero + " = " + conta.saldo);
-    }
+    
+        boolean saqueEfetuado = conta.realizarSaque(10);
 
+        if (saqueEfetuado){
+            System.out.println("Saque efetuado com sucesso");
+            System.out.println("Saldo atual da conta = " + conta.saldo);
+        } else {
+            System.out.println("Não foi possivel realizar saque. Saldo insuficiente");
+        }
+    
+        saqueEfetuado = conta.realizarSaque(500);
+        System.out.println("Tentativa de saque de 500 reais");
+        if (saqueEfetuado){
+            System.out.println("Saque efetuado com sucesso");
+            System.out.println("Saldo atual da conta = " + conta.saldo);
+        } else {
+            System.out.println("Não foi possivel realizar saque. Saldo insuficiente");
+        }
+
+        System.out.println("Depósto de 500 reais");
+        conta.depositar(500);
+        System.out.println("Saldo atual da conta = " + conta.saldo);
+    
+        if (conta.verificarUsoChequeEspecial()){
+            System.out.println("Está usando cheque especial");
+        } else {
+        System.out.println("Não está usando cheque especial");
+        }
+
+        conta.realizarSaque(600);
+        System.out.println("Saldo atual da conta = " + conta.saldo);
+        if (conta.verificarUsoChequeEspecial()){
+            System.out.println("Está usando cheque especial");
+        } else {
+        System.out.println("Não está usando cheque especial");
+        }
+    }
 }
+
--- aula27_exercicios/ContaCorrente.java
+++ aula27_exercicios/ContaCorrente.java
@@ -2,9 +2,43 @@ package aula27_exercicios;
 
 public class ContaCorrente {
 
-        String numero;
-        String agencia;
-        boolean especial;
-        double limiteEspecial;
-        double saldo;
+    String numero;
+    String agencia;
+    boolean especial;
+    double limiteEspecial;
+    double valorEspecialUsado;
+    double saldo;
+
+    boolean realizarSaque(double quantiaASacar){
+        
+        //tem saldo na conta
+        if (saldo >= quantiaASacar) {
+            saldo =+ quantiaASacar;
+            return true;
+        } else { //não tem saldo na conta
+            if (especial){
+                //verificar se o limite especial tem saldo
+                double limite = limiteEspecial + saldo;
+                if (limite >= quantiaASacar){
+                    saldo -= quantiaASacar;
+                    return true;
+                } else {
+                    return false; // não é especial e não tem saldo suficiente
+                    }
+            }
+        }
+        return especial;
+        
+    }
+
+    void depositar(double valorDepositado){
+            
+        saldo += valorDepositado;
+    } 
+
+    boolean verificarUsoChequeEspecial(){
+        return saldo < 0;
+    }
+
+
 }
