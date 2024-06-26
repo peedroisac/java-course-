89e3ed885dbcf185791c73716b313b99df2aa60b
pedro <peedroisac@gmail.com>
2024-06-26 15:37:40 -0300

    commit

---
+++ aula39/Aluno.java
@@ -0,0 +1,54 @@
+package aula39;
+
+public class Aluno extends Pessoa {
+
+    private String curso;
+    private double[] notas;
+
+    public void verificarAcesso(){
+
+        this.nomeVisibilidade
+
+    }
+
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String curso, double[] notas) {
+        super(nome, endereco, telefone);
+        this.curso = curso;
+        this.notas = notas;
+    }
+
+    public String getCurso() {
+
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia() {
+        return 0;
+    }
+
+    public boolean verificarAprovado() {
+        return true;
+    }
+
+    public void metodoQualquer(){
+        super.setCpf("2121212121");
+        //ou
+        this.setCpf("34343434");
+    }
+}
---
+++ aula39/Teste.java
@@ -0,0 +1,7 @@
+package aula39;
+
+public class Teste {
+
+
+
+}
